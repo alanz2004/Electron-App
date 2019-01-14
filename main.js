8f3681066f28a6d48ea15af498dc33f94d691b52
alanz2004 <42538134+alanz2004@users.noreply.github.com>
2019-01-14 17:19:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app,BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+//init win
+let win;
+
+function createWindow() {
+    //Create browser window
+    win = new BrowserWindow({width:800,height:600});
+
+    //Load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    //Open devtools
+    win.webContents.openDevTools()
+
+    win.on('closed',() => {
+        win = null;
+    });
+}
+
+//Run create window
+app.on('ready', createWindow);
+
+//Quit when all windows are closed
+app.on('window-all-closed',() => {
+ if(process.platform !== 'darwin'){
+     app.quit();
+ }
+});
---
+++ renderer.js
@@ -0,0 +1,1 @@
+//Some rendere File
